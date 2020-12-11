70b9127e086193c6f1060a0bb34941c2335bdce1
Dmitry Melikhov <dima.mel8@gmail.com>
2020-12-11 10:46:28 +0200

    Initialize project using Spring Initializr

---
+++ src/main/java/me/dibro/gallery/api/GalleryApiApplication.java
@@ -0,0 +1,12 @@
+package me.dibro.gallery.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GalleryApiApplication.class, args);
+    }
+}
---
+++ src/test/java/me/dibro/gallery/api/GalleryApiApplicationTests.java
@@ -0,0 +1,12 @@
+package me.dibro.gallery.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
